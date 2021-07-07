e4bd11a99f05fe527bc50c1498291fc23830410f
dhirajp504@gmail.com <dhirajp504@gmail.com>
2021-07-07 09:59:07 +0530

    Added: Take user input as Enter Last Name

--- src/regex/RegularExpressions.java
+++ src/regex/RegularExpressions.java
@@ -4,12 +4,28 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class EnterValidFirstName {
+public class RegularExpressions {
 	Scanner SC = new Scanner(System.in);
 
-	boolean First() {
+	boolean FirstEntity() {
 		String i;
 		System.out.println("Enter Your First Name :-");
+		i = SC.next();
+		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2}$");// Regex sintax
+		Matcher matcher = pattern.matcher(i);// Regex sintax
+
+		boolean matchFound = matcher.matches();// Use for identification of correct or incorrect.
+		if (matchFound) {
+			System.out.println("According To Pattern Matcher is Correct");
+		} else {
+			System.out.println("According To Pattern Matcher is Inorrect");
+		}
+		return matchFound;
+	}
+
+	boolean LastEntity() {
+		String i;
+		System.out.println("Enter Your Last Name :-");
 		i = SC.next();
 		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2}$");// Regex sintax
 		Matcher matcher = pattern.matcher(i);// Regex sintax
